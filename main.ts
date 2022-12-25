75ed1202045cb56776262a54338fee71fd1ec8f7
fishiversity <41026299+fishiversity@users.noreply.github.com>
2022-12-25 08:38:21 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    wuKong.setAllMotor(-80, 0)
+})
+input.onButtonPressed(Button.AB, function () {
+    wuKong.setAllMotor(-100, -100)
+})
+input.onButtonPressed(Button.B, function () {
+    wuKong.setAllMotor(0, -80)
+})
+input.onGesture(Gesture.Shake, function () {
+    wuKong.stopAllMotor()
 })
